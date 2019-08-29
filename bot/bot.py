cd506483e640fbb9c96c2e10724b80c3742fd317
Mohamed Ahmed <mohamedahmed201304@gmail.com>
2019-08-29 02:55:36 +0200

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -48,6 +48,7 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
 
+ 
 @bot.group()
 async def cool(ctx):
     """Says if a user is cool.
@@ -61,7 +62,64 @@ async def _bot(ctx):
     """Is the bot cool?"""
     await ctx.send('Yes, the bot is cool.')
 #code starting from here
+#write it here youssab
+# how r u guys so geniosity, how can I be geniosity like u guys
+@bot.command()
+async def solved(ctx , idd :int )
+	user_id = ctx.message.author.id
+  file = open("users/"+user_id+".txt","a+")
+	file.write(str(idd)+"\n")
+  await ctx.send('one more problem solved! ')
 
+topics=[("intro","Introductory Problems"),("sorting","Sorting and Searching"),("searching","Sorting and Searching"),("dp","Dynamic Programming"),("graph","Graph Algorithms"),("rq","Range Queries"),("tree","Tree Algorithms"),("math","Mathematics"),("string","String Algorithms"),("additional","Additional Problems")]
+  
+def getall(typ, l, r, solved):
+  problems = []
+  path = "problems/"+typ+".txt"
+  file = open(path , "r")
+  for problem in file.readlines(): #xD
+    pnum = problem.split(' ')[0]
+    pnum = pnum.split('/')[-1]
+    solvedcount=int(problems.split(' ')[1])
+    if pnum in solved:
+      pass
+    else if solvedcount < l or solvedcount > r:
+      pass
+    else:
+      problems.append(pnum)
+  return problems
+    
+  
+  
+  
+def pick1(l, r, solved):
+  problems = []
+  problems.append(1321) #مزاجى
+
+  
+def pick(problems,solved):
+	for typ,l,r in problems:
+		allp = get_all(typ,l,r,solved)
+		avail = []
+		for i in allp:
+			if i in solved or i in problems:
+				pass
+			else:
+				avail.append(i)
+		if len(avail) == 0:
+			pass
+		else:
+			cur=random.choice(avail)
+			problems.append(cur)
+
+  
+def getTopic(top):
+  top=top.lower()
+  for i,j in topics:
+    if top == i:
+      return j
+  return "Error"
+  
 @bot.command()
 async def problem(ctx, l: int, r: int):
 	if r < l:
@@ -70,6 +128,38 @@ async def problem(ctx, l: int, r: int):
 	if(l < 0):
 		await ctx.send("Numbers should be >= 0")
 		return
-	await ctx.send("success")
+  problem = pick1(l,r,getSolved(ctx.message.author.id))
 
+@bot.command()
+async def suggest(ctx, *args: str):
+  if len(args) == 0:
+    await ctx.send("Provide valid arguments")
+    return
+  try:
+  	n=int(args[0])
+  except:
+    await ctx.send("Provide valid arguments")
+    return
+	if len(args) != 3*n+1:
+    await ctx.send("Provide valid arguments")
+    return
+  types=[]
+  for i in range(n):
+    try:
+    	topic=args[1+3*i]
+    	l=int(args[2+3*i])
+      r=int(args[3+3*i])
+    except:
+      await ctx.send("Provide valid arguments")
+    	return
+    topic=getTopic(topic)
+    if topic == "Error":
+      await ctx.send("Provide valid arguments")
+    	return
+    types.append((topic,l,r))
+  problems=pick(types,getSolved(ctx.message.author.id))
+  random.shuffle(problems)
+  for i in problems:
+    await ctx.send("https://cses.fi/problemset/task/"+i)
+  
 bot.run('token')
